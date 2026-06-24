682d063ca82ccf113e3b2723b663a1bca9a83fec
Arif Waquar <waquararif149@gmail.com>
2026-06-24 07:52:13 +0530

    Initial project setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running successfully",
+  });
+});
+
+export default app;
